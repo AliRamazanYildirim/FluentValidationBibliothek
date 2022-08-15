4374727df2ef21a6795ed8387e28de78e94e20b5
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-08-15 11:10:07 +0300

    AdresseValidator erstellen

---
+++ FluentValidationMit.NetCore6.Web/FluentValidierer/AdresseValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using FluentValidationMit.NetCore6.Web.Models;
+
+namespace FluentValidationMit.NetCore6.Web.FluentValidierer
+{
+    public class AdresseValidator:AbstractValidator<Adresse>
+    {
+        public string NichtLeereNachricht { get; } = "{PropertyName}sfeld darf nicht leer sein";
+        public AdresseValidator()
+        {
+            RuleFor(x => x.Inhalt).NotEmpty().WithMessage(NichtLeereNachricht);
+            RuleFor(x => x.Provinz).NotEmpty().WithMessage(NichtLeereNachricht);
+            RuleFor(x => x.PostleitZahl).NotEmpty().WithMessage(NichtLeereNachricht).
+                MaximumLength(5).WithMessage("{PropertyName}sfeld kann maximal {MaxLength} sein");
+
+        }
+    }
+}
--- FluentValidationMit.NetCore6.Web/FluentValidierer/KundeValidierer.cs
+++ FluentValidationMit.NetCore6.Web/FluentValidierer/KundeValidierer.cs
@@ -5,20 +5,29 @@ namespace FluentValidationMit.NetCore6.Web.FluentValidierer
 {
     public class KundeValidierer:AbstractValidator<Kunde>
     {
-        public string NoEmptyMessage { get; } = "{PropertyName}sfeld darf nicht leer sein";
+        public string NichtLeereNachricht { get; } = "{PropertyName}sfeld darf nicht leer sein";
         public KundeValidierer()
         {
             //Die serverseitige Validierung wird von KundeValidator durchgeführt.
 
-            RuleFor(x => x.Name).NotEmpty().WithMessage(NoEmptyMessage);
-            RuleFor(x => x.Email).NotEmpty().WithMessage(NoEmptyMessage)
+            RuleFor(x => x.Name).NotEmpty().WithMessage("{PropertyName}nsfeld darf nicht leer sein");
+            RuleFor(x => x.Email).NotEmpty().WithMessage(NichtLeereNachricht)
                 .EmailAddress().WithMessage("Die E-Mail hat nicht das richtige Format");
-            RuleFor(x => x.Alter).NotEmpty().WithMessage(NoEmptyMessage)
+            RuleFor(x => x.Alter).NotEmpty().WithMessage(NichtLeereNachricht)
                 .InclusiveBetween(18, 60).WithMessage("Altersfeld muss zwischen 18 und 60 sein");
-            RuleFor(x => x.GeburtsDatum).NotEmpty().WithMessage(NoEmptyMessage).Must(x =>
+            RuleFor(x => x.GeburtsDatum).NotEmpty().WithMessage(NichtLeereNachricht).Must(x =>
             {
                 return DateTime.Now.AddYears(-18) >= x;
             }).WithMessage("Sie müssen über 18 Jahre alt sein, um auf diese Website zugreifen zu können");
+
+            RuleForEach(x => x.Adressen).SetValidator(new AdresseValidator());
+            //Bei KundeController in Create Methode könnte man so schreiben
+
+            //AdresseValidator adresseValidator = new AdresseValidator();
+            //kunde.Adresse.ToList().ForEach(XmlConfigurationExtensions =>
+            //{
+            //    adresseValidator.Validate(x);
+            //});
 
         }
     }
